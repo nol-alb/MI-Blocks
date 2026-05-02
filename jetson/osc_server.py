1eae05d3d21595c8dc52fca95e1c113098fb6816
nol-alb <noelalben21@gmail.com>
2026-05-02 09:28:46 -0400

    system test

--- jetson/osc_server.py
+++ jetson/osc_server.py
@@ -1,17 +1,23 @@
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.udp_client import SimpleUDPClient
 
 JETSON_IP = "10.42.0.1"
 OSC_PORT = 5005
+
+PD_PORT = 5006
+pd_client = SimpleUDPClient("127.0.0.1", PD_PORT)
 
 connected_xiaos = {}
 
 def handle_xiao(address, *args):
     xiao_id = address.split("/")[2]
+    sensor = address.split("/")[3]
     if xiao_id not in connected_xiaos:
         connected_xiaos[xiao_id] = True
         print(f"[NEW] XIAO connected: {xiao_id}")
-    print(f"[{xiao_id}] count → {args[0]}")
+    print(f"[{xiao_id}] {sensor} → {args[0]}")
+    pd_client.send_message(address, args[0])
 
 dispatcher = Dispatcher()
 dispatcher.map("/xiao/*", handle_xiao)
